675755ee5c790e72b51386452d10404202bba6b3
king midas <m.reda16420@gmail.com>
2026-02-28 22:19:19 +0200

    feat (blazor): add routable UI pages

--- mini-ecommerce.Blazor/Program.cs
+++ mini-ecommerce.Blazor/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using mini_ecommerce.Blazor;
+using mini-ecommerce.Blazor;
+using mini-ecommerce.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
+
+builder.Services.AddScoped<ApiClient>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<OrderService>();
 
 await builder.Build().RunAsync();
